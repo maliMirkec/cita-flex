27c8212383ddb0e7b5cd65e063b5ad3188c6fd00
Silvestar Bistrovic <malimirkeccita@gmail.com>
2016-03-04 11:43:21 +0100

    Fixing edge support.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/css/dist/normalize.min.css?<?php echo uniqid(); ?>">
+  <link rel="stylesheet" href="/css/dist/normalize.min.css?<?php echo uniqid(); ?>">  
   <link rel="stylesheet" href="/css/dist/cita-flex.min.css?<?php echo uniqid(); ?>">
   <title>CiTA FLeX</title>
   <style media="screen">
@@ -13,39 +13,44 @@
   </style>
 </head>
 <body>
-  <div class="flex-container flex-container-3 flex-full-page flex-fill flex-wrap">
-    <div class="flex-box flex-box-center">1</div>
-    <div class="flex-box flex-container flex-container-2 flex-fill flex-wrap">
-		  <div class="flex-box flex-box-center">1</div>
-		  <div class="flex-box flex-box-center">2</div>
-		  <div class="flex-box flex-box-center">3</div>
-		  <div class="flex-box flex-container flex-container-2 flex-fill flex-wrap">
-				<div class="flex-box flex-box-center">1</div>
-				<div class="flex-box flex-box-center">2</div>
-				<div class="flex-box flex-box-center">3</div>
-				<div class="flex-box flex-box-center">4</div>
-				<div class="flex-box flex-box-no-grow flex-box-center">5</div>
+  <!--[if lte IE 9]>
+    <p>
+      You are using outdated browser.
+    </p>
+  <![endif]-->
+  <div class="flex-container flex-container--3 flex-container--full-page flex-container--fill flex-container--wrap">
+    <div class="flex-box flex-center">1</div>
+    <div class="flex-box flex-container flex-container--2 flex-container--fill flex-container--wrap">
+		  <div class="flex-box flex-center">1</div>
+		  <div class="flex-box flex-center">2</div>
+		  <div class="flex-box flex-center">3</div>
+		  <div class="flex-box flex-container flex-container--2 flex-container--fill flex-container--wrap">
+				<div class="flex-box flex-center">1</div>
+				<div class="flex-box flex-center">2</div>
+				<div class="flex-box flex-center">3</div>
+				<div class="flex-box flex-center">4</div>
+				<div class="flex-box flex-box-no-grow flex-center">5</div>
 		  </div>
-		  <div class="flex-box flex-box-center">5</div>
-		  <div class="flex-box flex-box-center">6</div>
+		  <div class="flex-box flex-center">5</div>
+		  <div class="flex-box flex-center">6</div>
     </div>
-    <div class="flex-box flex-box-center">3</div>
-    <div class="flex-box flex-box-center">4</div>
-    <div class="flex-box flex-box-center">5</div>
-    <div class="flex-box flex-box-center">6</div>
-    <div class="flex-box flex-box-center">7</div>
-    <div class="flex-box flex-box-center">8</div>
-    <div class="flex-box flex-box-center">9</div>
-    <div class="flex-box flex-box-center">10</div>
-    <div class="flex-box flex-container flex-container-2 flex-fill flex-wrap">
-			<div class="flex-box flex-box-center">1</div>
-			<div class="flex-box flex-box-center">2</div>
-			<div class="flex-box flex-box-center">3</div>
-			<div class="flex-box flex-box-center">4</div>
-			<div class="flex-box flex-box-center">5</div>
-			<div class="flex-box flex-box-center">6</div>
+    <div class="flex-box flex-center">3</div>
+    <div class="flex-box flex-center">4</div>
+    <div class="flex-box flex-center">5</div>
+    <div class="flex-box flex-center">6</div>
+    <div class="flex-box flex-center">7</div>
+    <div class="flex-box flex-center">8</div>
+    <div class="flex-box flex-center">9</div>
+    <div class="flex-box flex-center">10</div>
+    <div class="flex-box flex-container flex-container--2 flex-container--fill flex-container--wrap">
+			<div class="flex-box flex-center">1</div>
+			<div class="flex-box flex-center">2</div>
+			<div class="flex-box flex-center">3</div>
+			<div class="flex-box flex-center">4</div>
+			<div class="flex-box flex-center">5</div>
+			<div class="flex-box flex-center">6</div>
     </div>
-    <div class="flex-box flex-box-center">12</div>
+    <div class="flex-box flex-center">12</div>
   </div>
 </body>
 </html>
