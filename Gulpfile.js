98e6a17b223f42094977d5db1b5c28f3dd7ab04c
Star Bist <malimirkeccita@gmail.com>
2017-04-19 10:47:09 +0200

    Package 1.2.6

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,30 +2,57 @@
 
 require('es6-promise').polyfill();
 
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var minifyCss    = require('gulp-minify-css');
-var rename       = require('gulp-rename');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 var gulpSequence = require('gulp-sequence');
- 
+var bump = require('gulp-bump');
+
+// Will patch the version
+gulp.task('bump:patch', function() {
+    return gulp.src(['./package.json', 'bower.json'])
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+// Will patch the version
+gulp.task('bump:minor', function() {
+    return gulp.src(['./package.json', 'bower.json'])
+    .pipe(bump({
+        type: 'minor'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+// Will patch the version
+gulp.task('bump:major', function() {
+    return gulp.src(['./package.json', 'bower.json'])
+    .pipe(bump({
+        type: 'major'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('dev', function() {
-  return gulp.src('./css/inc/*.scss')
+    return gulp.src('./css/inc/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./css/dev'));
 });
 
 gulp.task('dist', function() {
-  return gulp.src('./css/dev/*.css')
+    return gulp.src('./css/dev/*.css')
     .pipe(minifyCss())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./css/dist'));
 });
 
 gulp.task('default', gulpSequence('dev', 'dist'));
- 
+
 gulp.task('default:watch', function() {
-  gulp.watch(['./css/inc/*.scss', './css/src/*.scss'], ['dev']);
-  gulp.watch('./css/dev/*.css', ['dist']);
+    gulp.watch(['./css/inc/*.scss', './css/src/*.scss'], ['dev']);
+    gulp.watch('./css/dev/*.css', ['dist']);
 });
+
